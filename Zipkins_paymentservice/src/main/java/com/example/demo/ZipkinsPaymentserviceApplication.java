baf9a8621618a71ffef3341b04cc6d5f270f8e0f
Akabhi10 <145186232+Akabhi10@users.noreply.github.com>
2025-08-09 13:13:44 +0530

    Add files via upload

---
+++ Zipkins_paymentservice/src/main/java/com/example/demo/ZipkinsPaymentserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinsPaymentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinsPaymentserviceApplication.class, args);
+	}
+
+}
